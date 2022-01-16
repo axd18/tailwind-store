4f563fbe7500a6df578b88eb46cdc38abc98fba6
axd18 <adrianfernandezj@hotmail.com>
2022-01-16 14:56:11 -0300

    creando componente reutilizable del boton

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./assets/main.css"
 import App from "./App"
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./assets/main.css"
 
 // redux
 import { Provider } from "react-redux"
@@ -9,6 +11,11 @@ import store from "./data/store"
 
 ReactDOM.render(
   <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={App} />
+      </Routes>
+    </Router>
     <App />
   </Provider>,
   document.getElementById("root")
