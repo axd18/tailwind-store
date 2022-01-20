0de3248fd6926c741b8c2b810914d0a7c4af2d9e
axd18 <adrianfernandezj@hotmail.com>
2022-01-19 22:43:39 -0300

    navbar-3

--- src/index.js
+++ src/index.js
@@ -7,9 +7,26 @@ import App from "./App"
 import { Provider } from "react-redux"
 import store from "./data/store"
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import About from "./components/About"
+import Contact from "./components/Contact"
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById("root")
 )
