f64f928d0b43fe6da5ce7180953a560a37c6e7eb
adrian-axd <adrianfernandezj@hotmail.com>
2022-01-27 14:24:21 -0300

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,35 +5,37 @@ import Searchbar from './searchbar/Searchbar'
 
 const Header = ({ toggleOpen }) => {
   return (
-    
-      <header className='p-4 shadow-sm'>
-      <nav classNameName="">
-        <div className="container flex items-center justify-between">
+    <header className="p-4 shadow-sm">
+      <div className=" container block md:flex justify-between">
+        <div className=" container flex md:block justify-between">
           <Link to="/about">
             <h1>Logo</h1>
           </Link>
-          <Searchbar />
-          <Icons />
-        <div classNameName="px-4 cursor-pointer" onClick={toggleOpen}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            classNameName="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+
+          <div
+            className="w-8 h-8 cursor-pointer md:hidden"
+            onClick={toggleOpen}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              classNameName="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
         </div>
-        </div>
-      </nav>
-      </header>
-    
+        <Searchbar />
+        <Icons />
+      </div>
+    </header>
   )
 }
 
--- src/components/header/searchbar/Searchbar.js
+++ src/components/header/searchbar/Searchbar.js
@@ -2,15 +2,15 @@
 
 const Searchbar = () => {
     return ( 
-        <>
-            <div class="  max-w-l relative flex justify-center">
-                <span class="absolute left-4 top-3 text-lg text-gray-200">
+        <div className="">
+            <div class=" container w-full relative flex h-10">
+                <span class="absolute m-2 text-lg text-gray-200">
                     <i class="fas fa-search"></i>
                 </span>
                 <input type="text" class="w-full border border-primary border-r-0 pl-12 py-3 rounded-l-md focus:outline-none" placeholder="Buscar"/>
                 <button class="bg-gray-400 border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition">Buscar</button>
             </div>
-        </>
+        </div>
      );
 }
  
