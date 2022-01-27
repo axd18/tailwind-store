f5416c51befc187452d30bf648c48c392910d6e7
adrian-axd <adrianfernandezj@hotmail.com>
2022-01-27 07:56:07 -0300

    trabajando con el responsive del header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom'
-import Container from '../container/container.component'
+
+import Icons from './icons/Icons'
+import Searchbar from './searchbar/Searchbar'
 
 const Header = ({ toggleOpen }) => {
   return (
-    <Container>
-      <nav classNameName="container flex items-center justify-between">
-        {/* <div classNameName="px-4 cursor-pointer md:hidden" onClick={toggleOpen}>
+    
+      <header className='p-4 shadow-sm'>
+      <nav classNameName="">
+        <div className="container flex items-center justify-between">
+          <Link to="/about">
+            <h1>Logo</h1>
+          </Link>
+          <Searchbar />
+          <Icons />
+        <div classNameName="px-4 cursor-pointer" onClick={toggleOpen}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             classNameName="h-6 w-6"
@@ -20,64 +29,11 @@ const Header = ({ toggleOpen }) => {
               d="M4 6h16M4 12h16M4 18h7"
             />
           </svg>
-        </div> */}
-        <div className="container flex items-center justify-between">
-          <Link to="/about">
-            <h1>Logo</h1>
-          </Link>
-
-          <div className="w-full max-w-xl relative flex">
-            <span className="absolute left-4 top-3 text-lg text-gray-200">
-              <i className="fas fa-search"></i>
-            </span>
-            <input
-              type="text"
-              className="w-full border border-primary border-r-0 pl-12 py-3 rounded-l-md focus:outline-none"
-              placeholder="Buscar"
-            />
-            <button className="bg-primary border border-primary text-white px-8 rounded-r-md hover:bg-transparent hover:text-primary transition">
-              Buscar
-            </button>
-          </div>
-
-          <div className="flex items-center space-x-4">
-            <Link
-              to="/"
-              className="text-center text-gray-700 hover:text-primary transition relative"
-            >
-              <div className="txt-2xl">
-                <i className="far fa-heart"></i>
-              </div>
-              <div className="text-xs leading-3">Lista</div>
-              <span className="absolute -right-3 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-                8
-              </span>
-            </Link>
-            <Link
-              to="/about"
-              className="text-center text-gray-700 hover:text-primary transition relative"
-            >
-              <div className="txt-2xl">
-                <i className="fas fa-shopping-bag"></i>
-              </div>
-              <div className="text-xs leading-3">Carrito</div>
-              <span className="absolute -right-2 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-                8
-              </span>
-            </Link>
-            <Link
-              to="/"
-              className="text-center text-gray-700 hover:text-primary transition relative"
-            >
-              <div className="txt-2xl">
-                <i className="far fa-user"></i>
-              </div>
-              <div className="text-xs leading-3">Cuenta</div>
-            </Link>
-          </div>
+        </div>
         </div>
       </nav>
-    </Container>
+      </header>
+    
   )
 }
 
